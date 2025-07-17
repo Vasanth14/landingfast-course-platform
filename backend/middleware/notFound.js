96a1fe55878a2b1dfc5b6cfeb56de310708d5837
vasanth14 <vasanth1997sep@gmail.com>
2025-07-18 01:44:42 +0530

    fixes

--- backend/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-const AppError = require('../utils/appError');
-
-const notFound = (req, res, next) => {
-  const error = new AppError(`Not found - ${req.originalUrl}`, 404);
-  next(error);
-};
-
-module.exports = notFound;
