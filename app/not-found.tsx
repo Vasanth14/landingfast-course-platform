b56a4cba6ac8098d6f4565daa83068688f2dd738
vasanth14 <vasanth1997sep@gmail.com>
2025-07-25 21:44:09 +0530

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/'); // Redirect to landing page
+  }, [router]);
+
+  return null;
+} 
